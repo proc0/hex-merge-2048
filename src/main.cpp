893a08ef4122efc72df5e1a684dc253adc21c590
proc0 <alanvx@gmail.com>
2026-07-20 11:35:42 -0700

    adding notes

--- src/main.cpp
+++ src/main.cpp
@@ -12,28 +12,29 @@ int main(void){
     return 0;
 }
 
-// LAB: For menus, Surface should have one layout method
-// with a shell Clay element that calls the different 
-// layout layers within Surface (i.e. layoutMainMenu, or
-// layoutPauseMenu). This would allow transition effects
-// to work when changing UI screens. It might need
-// refactoring of Layout member function pointers and
-// how App is referencing them. Watchout for perf issues.
+// MODE: time based mode "survival mode"
+// new chips spawn at a time interval
+// time interval accelerates as a factor of both maxValue and time
+// goes on beyond 2048?
 
-// NOTE: This could be a vector/stack of Layouts/Layers that
-// extend and abstract class, and Surface would loop
-// through the layers calling their methods, removing and
-// adding Layouts/Layers as needed. Each Layout would use
-// Widget to render its components glueing them together.
+// MODE: classic mode "turn-based"
+// chips spawn on every move that has a merge
 
-// LAB: Breakup Surface into Display and Layout classes,
-// or just break up Surface into multiple files. Using
-// different classes could allow composition between
-// Layout, Widget, and Display, with Surface containing
-// the methods equivalent for Pages or Screens.
+// MODE: decaying/hardened mode 
+// chips slowly harden so you can't merge them anymore
+// higher number chips harden slowly
 
-// TODO: Add support for multiple scrollbars by enhancing
-// how Surface and Widget handle them
+// MODE: LAB: music/rythm based mode
+// chips spawn on the beat?
+// moving is done on the beat?
+// could be combined with time based?
 
-// LAB: Look into transition Clay_Text to allow for better 
-// transitioning effects with elements containing text.
+// TODO: move gridlock logic to somewhere that does not depend on move inputs
+// right now the game over screen only shows up after pressing a key
+
+// TODO: scale chip numbers with screen
+// TODO: scale chip size when moving (get bigger 1.1f scale)
+// LAB: set a render target for chip backgrounds, only render text and effects every frame
+// cache the chip backgrounds for every number
+
+// TODO: scale spawning chips, i.e. like they are landing on the board
